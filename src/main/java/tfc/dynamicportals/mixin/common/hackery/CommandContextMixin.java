d5732f7e3e28bde8c826ab56e796b689e7ef1711
GiantLuigi4 <49770992+GiantLuigi4@users.noreply.github.com>
2022-06-29 18:36:46 -0400

    yeet

--- src/main/java/tfc/dynamicportals/mixin/common/hackery/CommandContextMixin.java
+++
@@ -1,9 +0,0 @@
-package tfc.dynamicportals.mixin.common.hackery;
-
-import com.mojang.brigadier.context.CommandContext;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(CommandContext.class)
-public class CommandContextMixin<T> {
-
-}
