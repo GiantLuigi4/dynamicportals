2c2a74cf9ce22cdc641ca3987d5d98e04d659740
GiantLuigi4 <49770992+GiantLuigi4@users.noreply.github.com>
2022-07-02 15:11:36 -0400

    don't you love when an import gets deleted for no apparent reason

--- src/main/java/tfc/dynamicportals/mixin/client/optimization/VBOMixin.java
+++ src/main/java/tfc/dynamicportals/mixin/client/optimization/VBOMixin.java
@@ -4,6 +4,7 @@ import com.mojang.blaze3d.vertex.BufferBuilder;
 import com.mojang.blaze3d.vertex.VertexBuffer;
 import com.mojang.blaze3d.vertex.VertexFormat;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
